8bd5e1d807daf9526ada85c6d18c31faf9943492
divyansh-gupta <divyg@vt.edu>
2016-04-03 21:50:46 -0400

    Add empty User model

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require('express');
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
+require('./src/lib/mongoose');
+
 var app = express();
 var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler,  {
+app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
